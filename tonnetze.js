367bdffc338109837bcf4051ac7a9d8be7bc6dd8
Vincent <vincent.dore.millet@protonmail.com>
2019-05-03 19:45:41 -0400

    Let's start somewhere

---
+++ tonnetze.js
@@ -0,0 +1,10 @@
+// review chord coordinates in graph
+// all positive chord edge should lead to a major chord
+// all negative chord edge should lead to a minor chord
+
+// chord polarity
+    // define scales visited during progression
+    // used to identify what modulations are relevant and how likely they are
+    
+// define elasticity
+    // number of different scales authorized to explore in a single progression
