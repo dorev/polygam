e2dbabb52549a4273d2b5ddba92bf5c6d62e550d
Vincent <vincent.dore.millet@protonmail.com>
2019-05-03 20:48:50 -0400

    Bringing in more from CodePen

---
+++ sequencer.js
@@ -0,0 +1,451 @@
+//-------------------------------------------------
+// Custom sequencer element
+//-------------------------------------------------
+customElements.define("custom-sequencer", class extends HTMLElement
+{
+  constructor()
+  {
+    super();
+    
+    this.currentBar = -1;
+    this.currentBeat = -1;
+    this.noteDuration = this.hasAttribute("note-duration") ? this.getAttribute("note-duration") : "4n";
+    this.seqLength = this.hasAttribute("seq-length") ? Number(this.getAttribute("seq-length")) : 4;
+    this.seqHeight = this.hasAttribute("seq-height") ? Number(this.getAttribute("seq-height")) : 4;
+    
+    // Structure holding the whole score
+    this.score = { "beats" : [], "bars" : [] };    
+    for(let i = 0; i < this.seqLength; ++i)
+    {
+      this.score.beats.push(Array(this.seqHeight).fill(0));
+    }        
+    /* this.score expected structure
+    { 
+      "beats" : [
+        [0,0,0,0],  // note activation states of beat 0
+        [0,0,0,0],  // beat 1
+        [0,0,0,0],  // ...
+        [0,0,0,0]
+      ]
+      "bars" : [
+        ["C4","D#4","G4","C5"], // notes with octave (sharps only, no flats) of bar 0
+        ["G4","B4","D5","F5"]   // bar 1
+      ]                         // ...
+    }
+    */
+      
+    //--------------------------------------------------------
+    // CSS style
+    //--------------------------------------------------------
+    let style = document.createElement('style');
+    style.textContent =`
+    .custom-sequencer-song 
+    {
+      display: flex;      
+      position: relative;
+      height: 100%;
+      width: 100%;
+      border: 0.05em solid #999;
+      border-radius : 0.25em;
+      padding:  0.2em;
+      background-image: linear-gradient(45deg, #444, #333);
+    }
+
+    .custom-sequencer-bar 
+    {
+      position: absolute;
+      display: flex;
+      border-radius : inherit;
+      padding:  0.05em;
+      top : 0.1em;
+      bottom : 0.1em;
+      left : 0.1em;
+      right : 0.1em;
+      background-image: linear-gradient(135deg, #333, #555);
+    }
+
+    .custom-sequencer-beat 
+    {
+      display: flex;
+      flex-direction: column-reverse;
+      width: 100%;
+      margin: 0 0.1em 0 0.1em;
+      border-radius : inherit;
+    }
+
+    .custom-sequencer-note
+    {  
+      display: flex;
+      flex-direction: column;
+      height: 95%;
+      width: 95%;
+      border: 0.05em solid #aaa;
+      border-radius : inherit;
+      margin: 0.1em;
+      background-image: linear-gradient(225deg, #333, #555);
+      opacity: 0.3;
+    }
+
+    .custom-sequencer-note.active
+    {  
+      background-image: linear-gradient(45deg, #888, #aaa);
+      opacity: 0.5;
+    }
+
+    .custom-sequencer-note:hover
+    {  
+      opacity: 0.9;
+    }
+
+    .custom-sequencer-note.root
+    {  
+      background : #eee;
+    }
+
+    .custom-sequencer-beat.active .custom-sequencer-note
+    {  
+      opacity: 1;
+    }
+
+    .custom-sequencer-note.active.C  
+    {  
+      background-image: linear-gradient(#FF3333, #FF8E33);
+    }
+
+    .custom-sequencer-note.active.Cs  
+    {  
+      background-image: linear-gradient(#FFB833, #E4FF33);
+    }
+
+    .custom-sequencer-note.active.D  
+    {  
+      background-image: linear-gradient(#E4FF33, #94FF33);
+    }
+
+    .custom-sequencer-note.active.Ds  
+    {  
+      background-image: linear-gradient(#94FF33, #33FF71);
+    }
+
+    .custom-sequencer-note.active.E  
+    {  
+      background-image: linear-gradient(#33FF71, #33FFC0);
+    }
+
+    .custom-sequencer-note.active.F  
+    {  
+      background-image: linear-gradient(#33FFC0, #33FFAA);
+    }
+
+    .custom-sequencer-note.active.Fs  
+    {  
+      background-image: linear-gradient(#33FFAA, #33F6FF);
+    }
+
+    .custom-sequencer-note.active.G 
+    {  
+      background-image: linear-gradient(#33F6FF, #337CFF);
+    }
+
+    .custom-sequencer-note.active.Gs 
+    {  
+      background-image: linear-gradient(#337CFF, #4833FF);
+    }
+
+    .custom-sequencer-note.active.A 
+    {  
+      background-image: linear-gradient(#4833FF, #A733FF);
+    }
+
+    .custom-sequencer-note.active.As 
+    {  
+      background-image: linear-gradient(#A733FF, #FF33E1);
+    }
+
+    .custom-sequencer-note.active.B 
+    {  
+      background-image: linear-gradient(#FF33E1, #FF3333);
+    }      
+    `;  
+    
+    //--------------------------------------------------------
+    // Construct default custom element
+    //--------------------------------------------------------
+    let shadow = this.attachShadow({mode: 'open'});  
+    shadow.appendChild(style);  
+    
+    this.song = document.createElement("div");
+    this.song.setAttribute("class","custom-sequencer-song");    
+    shadow.appendChild(this.song);      
+  }
+  
+  removeBar(barToRemove)
+  {  
+    this.stop();
+    
+    if(barToRemove >= 0 && barToRemove < this.song.children.length)
+    {
+      this.song.removeChild(this.song.children[barToRemove]);
+    }
+    
+    // Rename bar id of all subsequent notes
+    for(let barIndex = barToRemove; barIndex < this.song.children.length; ++barIndex)
+    {
+      let beatCount = this.getBarDOM(bar).children.length;
+      for(let beatIndex = 0; beatIndex < beatCount; ++beatIndex)
+      {
+        let noteCount = this.getBeatDOM(barIndex, beatIndex).children.length;
+        for(let noteIndex = 0; noteIndex < noteCount; ++noteIndex)
+        {
+          this.getNoteDOM(barIndex, beatIndex, noteIndex).setAttribute("bar",barIndex);
+        }
+      }
+    }
+    
+    // Remove from score
+    this.score.bars.splice(barToRemove,1);
+    
+  }
+  
+  stop()
+  {
+    if(this.currentBeat < 0 || this.CurrentBar < 0) { return; }
+    this.getBeatDOM(this.currentBar,this.currentBeat).setAttribute("class", "custom-sequencer-beat"); 
+    this.currentBeat = -1;
+    this.currentBar = -1;
+  }
+  
+  nextBeat()
+  {  
+    // Skip clearing last beat when song starts
+    if(this.currentBar >= 0)
+    {
+      this.getBeatDOM(this.currentBar,this.currentBeat).setAttribute("class", "custom-sequencer-beat");      
+    }
+    else
+    {
+      ++this.currentBar;
+    }    
+        
+    // If we reached the end of the bar
+    if(++this.currentBeat === this.getBarDOM(this.currentBar).children.length)
+    {
+      this.currentBeat = 0;
+      
+      // If we reached the end of the song
+      if(++this.currentBar === this.song.children.length)
+      {
+        this.currentBar = 0;
+      }  
+    } 
+      
+    this.getBeatDOM(this.currentBar,this.currentBeat).setAttribute("class", "custom-sequencer-beat active"); 
+    this.bringBarOnTop(this.currentBar);   
+  }
+  
+    
+  bringBarOnTop(barIndex)
+  {
+    for(let i = 0; i < this.score.bars.length; ++i)
+    {
+      this.getBarDOM(i).style.zIndex = (i === barIndex ? 1 : 0);
+    }
+  }
+
+  appendBar(notes)
+  { 
+    // Stop the sequencer to avoid unpredictable state
+    this.stop(); 
+    
+    let bar = document.createElement("div");
+    bar.setAttribute("class", "custom-sequencer-bar");
+    
+    for(let beatIndex = 0; beatIndex < this.seqLength; ++beatIndex)
+    {               
+      bar.appendChild(this.buildBeatDOM(this.song.children.length, beatIndex, notes));
+    }
+    
+    // Update DOM and score
+    this.song.appendChild(bar);     
+    this.score.bars.push(notes);
+  }
+  
+  buildBeatDOM(barIndex, beatIndex, notes)
+  {
+    // Create DOM element
+    let beat = document.createElement("div");
+    beat.setAttribute("class", "custom-sequencer-beat");
+
+    for(let i = 0; i < this.seqHeight; ++i)
+    {
+      beat.appendChild(this.buildNoteDOM(barIndex, beatIndex, i, notes[i]));
+    }
+    
+    return beat;
+  }  
+  
+  buildNoteDOM(barIndex, beatIndex, noteIndex, noteName ="")
+  {
+    // Create DOM element
+    let note = document.createElement("div");    
+    note.setAttribute("class", "custom-sequencer-note");     
+    note.setAttribute("bar", barIndex); 
+    note.setAttribute("beat", beatIndex); 
+    note.setAttribute("index", noteIndex);    
+    if(noteName.length) {note.setAttribute("note", noteName.replace(/s|S/,"#"));}  
+    
+    // Mouse and touch event listeners
+    note.addEventListener("click", this.noteClick.bind(this));
+    note.addEventListener("touch", this.noteClick.bind(this));
+    
+    return note;
+  }
+  
+  getBarDOM(barIndex)
+  {
+    return this.song.children[barIndex];
+  }
+  
+  getBeatDOM(barIndex, beatIndex)
+  {
+    return this.song
+      .children[barIndex]
+      .children[beatIndex];
+  }
+  
+  getNoteDOM(barIndex, beatIndex, noteIndex)
+  {
+    return this.song
+      .children[barIndex]
+      .children[beatIndex]
+      .children[noteIndex];
+  }
+  
+  noteClick(e)
+  {
+    let beatIndex = Number(e.target.getAttribute("beat"));
+    let noteIndex = Number(e.target.getAttribute("index"));    
+    let noteActivation = /custom-sequencer-note active .*/.test(e.target.getAttribute("class"));
+    
+    this.setNoteActivation(beatIndex, noteIndex, !noteActivation);
+  }
+  
+  setNoteActivation(beatIndex, noteIndex, noteActivation)
+  {    
+    // Update DOM
+    for(let barIndex = 0; barIndex < this.score.bars.length; ++barIndex)
+    {
+      let noteToEdit = this.getNoteDOM(barIndex, beatIndex, noteIndex);
+      
+      if(noteActivation)
+      {
+        if(noteToEdit.hasAttribute("note"))
+        {
+          noteToEdit.setAttribute
+          ("class", "custom-sequencer-note active " + noteToEdit.getAttribute("note").match(/[a-gA-G|s|S]{1,2}/)[0]);
+        }
+        else
+        {
+          noteToEdit.setAttribute("class", "custom-sequencer-note active");
+        } 
+      } 
+      else
+      {
+        noteToEdit.setAttribute("class", "custom-sequencer-note");
+      }      
+    }
+    
+    // Update score
+    this.score.beats[beatIndex][noteIndex] = noteActivation;
+  }
+  
+  getCurrentNotes()
+  {
+    if(this.currentBar < 0 || this.currentBeat < 0) { return [];}
+    let result = [];
+    
+    for(let noteIndex = 0; noteIndex < this.seqHeight; ++noteIndex)
+    {
+      if(this.score.beats[this.currentBeat][noteIndex] 
+        && this.score.bars[this.currentBar][noteIndex] != undefined) 
+      { 
+        result.push(this.score.bars[this.currentBar][noteIndex]);
+      }
+    }
+    
+    return result;
+  } 
+  
+  setBarNotes(barIndex, notes) 
+  {
+    this.score.bars[barIndex] = notes;
+    
+    // Update DOM
+    let bar = this.getBarDOM(barIndex);
+    
+    for(let beatIndex = 0; beatIndex < this.seqLength; ++beatIndex)
+    {               
+      for(let noteIndex = 0; noteIndex < this.seqHeight; ++noteIndex)
+      {
+        let note = this.getNoteDOM(barIndex, beatIndex, noteIndex);
+        if(noteIndex >= notes.length) { continue; }
+        if(/[a-gA-G|s|S]{1,2}/.test(notes[noteIndex]))
+        {
+          note.setAttribute("note", notes[noteIndex].replace(/s|S/,"#"));  
+        }      
+      }
+    }
+  } 
+  
+  setBeats(beats)
+  {
+    for(let beatIndex = 0; beatIndex < this.seqLength; ++beatIndex)
+    {
+      for(let noteIndex = 0; noteIndex < this.seqHeight; ++noteIndex)
+      {
+        if(beatIndex >= beats.length 
+           || noteIndex >= beats[beatIndex].length 
+           || beats[beatIndex][noteIndex] === undefined) { continue; }
+        
+        this.setNoteActivation(beatIndex, noteIndex, beats[beatIndex][noteIndex]);
+      }
+    }
+  }  
+
+});
+
+
+//-------------------------------------------------
+// Demo
+//-------------------------------------------------
+
+let seq = document.querySelector("custom-sequencer");
+seq.appendBar(["C#4","E4","G#4","B4","E5","G#5","B5"]);
+seq.appendBar(["F#3","A3","C4","F#4","A4","C5","F#5"]);
+seq.setBeats([[1],[,1],[,,1],[,1,,1],[],[,,,,1,,1],[,,,1,,1]]);
+
+let synth = new Tone.PolySynth(8, Tone.Synth).toMaster();
+let tempo = 256;
+let sequencerInterval;
+let sequencerRunning = false;
+
+function startSequencer()
+{
+  if(sequencerRunning) { return; }
+  sequencerRunning = true;
+  
+  sequencerInterval = setInterval(()=>
+  {
+    seq.nextBeat();
+    let notes = seq.getCurrentNotes();
+    synth.triggerAttackRelease(notes, seq.noteDuration);  
+    if(notes.length) {console.log(notes);}
+  },60000/tempo);  
+}
+
+function stopSequencer()
+{
+  clearInterval(sequencerInterval);
+  sequencerRunning = false;
+  seq.stop();
+}
--- tonnetze.js
+++ tonnetze.js
@@ -5,6 +5,233 @@
 // chord polarity
     // define scales visited during progression
     // used to identify what modulations are relevant and how likely they are
-    
+
 // define elasticity
     // number of different scales authorized to explore in a single progression
+
+
+const tonnetze = JSON.parse("[{\"edges\":{\"+x\":7,\"+y\":4,\"+z\":9,\"-x\":5,\"-y\":8,\"-z\":3},\"id\":0,\"type\":\"note\",\"neighbors\":[7,4,9,5,8,3],\"chords\":[12,13,14,15,16,17]},{\"edges\":{\"+x\":8,\"+y\":5,\"+z\":10,\"-x\":6,\"-y\":9,\"-z\":4},\"id\":1,\"type\":\"note\",\"neighbors\":[8,5,10,6,9,4],\"chords\":[18,19,20,21,22,23]},{\"edges\":{\"+x\":9,\"+y\":6,\"+z\":11,\"-x\":7,\"-y\":10,\"-z\":5},\"id\":2,\"type\":\"note\",\"neighbors\":[9,6,11,7,10,5],\"chords\":[24,25,26,27,28,29]},{\"edges\":{\"+x\":10,\"+y\":7,\"+z\":0,\"-x\":8,\"-y\":11,\"-z\":6},\"id\":3,\"type\":\"note\",\"neighbors\":[10,7,0,8,11,6],\"chords\":[13,17,30,31,32,33]},{\"edges\":{\"+x\":11,\"+y\":8,\"+z\":1,\"-x\":9,\"-y\":0,\"-z\":7},\"id\":4,\"type\":\"note\",\"neighbors\":[11,8,1,9,0,7],\"chords\":[12,14,19,23,34,35]},{\"edges\":{\"+x\":0,\"+y\":9,\"+z\":2,\"-x\":10,\"-y\":1,\"-z\":8},\"id\":5,\"type\":\"note\",\"neighbors\":[0,9,2,10,1,8],\"chords\":[15,16,18,20,25,29]},{\"edges\":{\"+x\":1,\"+y\":10,\"+z\":3,\"-x\":11,\"-y\":2,\"-z\":9},\"id\":6,\"type\":\"note\",\"neighbors\":[1,10,3,11,2,9],\"chords\":[21,22,24,26,31,33]},{\"edges\":{\"+x\":2,\"+y\":11,\"+z\":4,\"-x\":0,\"-y\":3,\"-z\":10},\"id\":7,\"type\":\"note\",\"neighbors\":[2,11,4,0,3,10],\"chords\":[12,13,27,28,30,35]},{\"edges\":{\"+x\":3,\"+y\":0,\"+z\":5,\"-x\":1,\"-y\":4,\"-z\":11},\"id\":8,\"type\":\"note\",\"neighbors\":[3,0,5,1,4,11],\"chords\":[16,17,18,19,32,34]},{\"edges\":{\"+x\":4,\"+y\":1,\"+z\":6,\"-x\":2,\"-y\":5,\"-z\":0},\"id\":9,\"type\":\"note\",\"neighbors\":[4,1,6,2,5,0],\"chords\":[14,15,22,23,24,25]},{\"edges\":{\"+x\":5,\"+y\":2,\"+z\":7,\"-x\":3,\"-y\":6,\"-z\":1},\"id\":10,\"type\":\"note\",\"neighbors\":[5,2,7,3,6,1],\"chords\":[20,21,28,29,30,31]},{\"edges\":{\"+x\":6,\"+y\":3,\"+z\":8,\"-x\":4,\"-y\":7,\"-z\":2},\"id\":11,\"type\":\"note\",\"neighbors\":[6,3,8,4,7,2],\"chords\":[26,27,32,33,34,35]},{\"edges\":{\"-x\":13,\"-y\":14,\"-z\":35},\"id\":12,\"type\":\"chord\",\"voicing\":\"major\",\"root\":0,\"notes\":[0,7,4],\"fifth\":7,\"third\":4,\"neighbors\":[13,14,35]},{\"edges\":{\"+x\":12,\"+z\":17,\"+y\":30},\"id\":13,\"type\":\"chord\",\"voicing\":\"minor\",\"root\":0,\"notes\":[0,7,3],\"fifth\":7,\"third\":3,\"neighbors\":[12,17,30]},{\"edges\":{\"+y\":12,\"+z\":15,\"+x\":23},\"id\":14,\"type\":\"chord\",\"voicing\":\"minor\",\"third\":0,\"notes\":[0,4,9],\"fifth\":4,\"root\":9,\"neighbors\":[12,15,23]},{\"edges\":{\"-z\":14,\"-x\":16,\"-y\":25},\"id\":15,\"type\":\"chord\",\"voicing\":\"major\",\"fifth\":0,\"notes\":[0,9,5],\"third\":9,\"root\":5,\"neighbors\":[14,16,25]},{\"edges\":{\"+x\":15,\"+y\":17,\"+z\":18},\"id\":16,\"type\":\"chord\",\"voicing\":\"minor\",\"fifth\":0,\"notes\":[0,5,8],\"root\":5,\"third\":8,\"neighbors\":[15,17,18]},{\"edges\":{\"-y\":16,\"-z\":13,\"-x\":32},\"id\":17,\"type\":\"chord\",\"voicing\":\"major\",\"third\":0,\"notes\":[0,8,3],\"root\":8,\"fifth\":3,\"neighbors\":[16,13,32]},{\"edges\":{\"-x\":19,\"-y\":20,\"-z\":16},\"id\":18,\"type\":\"chord\",\"voicing\":\"major\",\"root\":1,\"notes\":[1,8,5],\"fifth\":8,\"third\":5,\"neighbors\":[19,20,16]},{\"edges\":{\"+x\":18,\"+z\":23,\"+y\":34},\"id\":19,\"type\":\"chord\",\"voicing\":\"minor\",\"root\":1,\"notes\":[1,8,4],\"fifth\":8,\"third\":4,\"neighbors\":[18,23,34]},{\"edges\":{\"+y\":18,\"+z\":21,\"+x\":29},\"id\":20,\"type\":\"chord\",\"voicing\":\"minor\",\"third\":1,\"notes\":[1,5,10],\"fifth\":5,\"root\":10,\"neighbors\":[18,21,29]},{\"edges\":{\"-z\":20,\"-x\":22,\"-y\":31},\"id\":21,\"type\":\"chord\",\"voicing\":\"major\",\"fifth\":1,\"notes\":[1,10,6],\"third\":10,\"root\":6,\"neighbors\":[20,22,31]},{\"edges\":{\"+x\":21,\"+y\":23,\"+z\":24},\"id\":22,\"type\":\"chord\",\"voicing\":\"minor\",\"fifth\":1,\"notes\":[1,6,9],\"root\":6,\"third\":9,\"neighbors\":[21,23,24]},{\"edges\":{\"-y\":22,\"-z\":19,\"-x\":14},\"id\":23,\"type\":\"chord\",\"voicing\":\"major\",\"third\":1,\"notes\":[1,9,4],\"root\":9,\"fifth\":4,\"neighbors\":[22,19,14]},{\"edges\":{\"-x\":25,\"-y\":26,\"-z\":22},\"id\":24,\"type\":\"chord\",\"voicing\":\"major\",\"root\":2,\"notes\":[2,9,6],\"fifth\":9,\"third\":6,\"neighbors\":[25,26,22]},{\"edges\":{\"+x\":24,\"+z\":29,\"+y\":15},\"id\":25,\"type\":\"chord\",\"voicing\":\"minor\",\"root\":2,\"notes\":[2,9,5],\"fifth\":9,\"third\":5,\"neighbors\":[24,29,15]},{\"edges\":{\"+y\":24,\"+z\":27,\"+x\":33},\"id\":26,\"type\":\"chord\",\"voicing\":\"minor\",\"third\":2,\"notes\":[2,6,11],\"fifth\":6,\"root\":11,\"neighbors\":[24,27,33]},{\"edges\":{\"-z\":26,\"-x\":28,\"-y\":35},\"id\":27,\"type\":\"chord\",\"voicing\":\"major\",\"fifth\":2,\"notes\":[2,11,7],\"third\":11,\"root\":7,\"neighbors\":[26,28,35]},{\"edges\":{\"+x\":27,\"+y\":29,\"+z\":30},\"id\":28,\"type\":\"chord\",\"voicing\":\"minor\",\"fifth\":2,\"notes\":[2,7,10],\"root\":7,\"third\":10,\"neighbors\":[27,29,30]},{\"edges\":{\"-y\":28,\"-z\":25,\"-x\":20},\"id\":29,\"type\":\"chord\",\"voicing\":\"major\",\"third\":2,\"notes\":[2,10,5],\"root\":10,\"fifth\":5,\"neighbors\":[28,25,20]},{\"edges\":{\"-x\":31,\"-y\":13,\"-z\":28},\"id\":30,\"type\":\"chord\",\"voicing\":\"major\",\"root\":3,\"notes\":[3,10,7],\"fifth\":10,\"third\":7,\"neighbors\":[31,13,28]},{\"edges\":{\"+x\":30,\"+z\":33,\"+y\":21},\"id\":31,\"type\":\"chord\",\"voicing\":\"minor\",\"root\":3,\"notes\":[3,10,6],\"fifth\":10,\"third\":6,\"neighbors\":[30,33,21]},{\"edges\":{\"+x\":17,\"+y\":33,\"+z\":34},\"id\":32,\"type\":\"chord\",\"voicing\":\"minor\",\"fifth\":3,\"notes\":[3,8,11],\"root\":8,\"third\":11,\"neighbors\":[17,33,34]},{\"edges\":{\"-y\":32,\"-z\":31,\"-x\":26},\"id\":33,\"type\":\"chord\",\"voicing\":\"major\",\"third\":3,\"notes\":[3,11,6],\"root\":11,\"fifth\":6,\"neighbors\":[32,31,26]},{\"edges\":{\"-x\":35,\"-y\":19,\"-z\":32},\"id\":34,\"type\":\"chord\",\"voicing\":\"major\",\"root\":4,\"notes\":[4,11,8],\"fifth\":11,\"third\":8,\"neighbors\":[35,19,32]},{\"edges\":{\"+x\":34,\"+z\":12,\"+y\":27},\"id\":35,\"type\":\"chord\",\"voicing\":\"minor\",\"root\":4,\"notes\":[4,11,7],\"fifth\":11,\"third\":7,\"neighbors\":[34,12,27]}]");
+
+// Freeze tonnetze
+tonnetze.forEach(node => Object.freeze(node.edges));
+Object.freeze(tonnetze);
+
+const noteNamesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+const noteNamesSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+Object.freeze(noteNamesFlat);
+Object.freeze(noteNamesSharp);
+
+//---------------------------------------------------------------
+// Scale patterns
+//---------------------------------------------------------------
+
+const scalePatterns = {
+  major:      [["+x"],["-x"],["+y"],["+z"],["+y","+x"],["+x","+x"]],
+  minor:      [["+x"],["-x"],["-y"],["-z"],["-x","-x"],["+x","+x"]],
+  pentaMajor: [["+x"],["+y"],["+z"],["+x","+x"]],
+  pentaMinor: [["+x"],["-x"],["-z"],["-x","-x"],],
+  blues:      [["-x"],["+x"],["-z"],["-z","+x"],["-z","-z"]]
+}
+Object.freeze(scalePatterns);
+
+const scaleChordsPatterns = {
+  major: { 
+    second:["-y","-z","-y"],
+    third:["+z"],
+    fourth:["-y","-z"],
+    fifth:["+z","+y"],
+    sixth:["-y"]
+  },
+  minor: { 
+    second:["+y","+z","+y"],
+    third:["-z"],
+    fourth:["+y","+z"],
+    fifth:["-z","-y"],
+    sixth:["+y"]
+  }
+}
+Object.freeze(scaleChordsPatterns);
+
+
+// Retrieve notes according to scale pattern
+function getScaleNotes(root, scale)
+{  
+  let scaleNotes = [root];    
+  scalePatterns[scale].forEach( pattern => {
+    //let currentNode = root;
+    //pattern.forEach(move => currentNode = tonnetze[currentNode].edges[move]);
+    scaleNotes.push(tonnetze[tonnetzeMove(root, pattern)].id);
+  });
+  return scaleNotes.sort((a,b) => a - b);
+}
+
+//---------------------------------------------------------------
+// Chord patterns
+//---------------------------------------------------------------
+
+const chordPatterns = {
+  major:      [["+x"],["+y"]],
+  minor:      [["+x"],["-z"]],
+  augmented:  [["+y"],["+y","+y"]],
+  diminished: [["-z"],["-z","-z"]],
+  extensions : {
+    m7th:       [["+x"],["-z"]],
+    M7th:       [["+x"],["+y"]],
+    M9th:       [["+x"],["+x"]],
+    m6th:       [["-y"]],
+    M6th:       [["+z"]]
+  }
+}
+
+Object.freeze(chordPatterns.extensions);
+Object.freeze(chordPatterns);
+
+// Find scale major and minor chords
+function getScaleChords(root, scale)
+{
+  // List all chord node of each note
+  // Count each chord appearance
+  // Keep only chords with 3 appearances
+  let chordsFound = [];
+  let scaleChords = [];
+  
+  // Get scales notes 
+  let scaleNotes = getScaleNotes(root, scale);  
+  scaleNotes.forEach(note => tonnetze[note].chords.forEach(chord => chordsFound.push(chord)));  
+  chordsFound.sort((a,b) => a - b);
+  
+  let chordCount = 0;  
+  let currentChord = -1;
+  for(let i = 0; i < chordsFound.length; ++i)
+  {
+    if(i === 0) 
+    { 
+      currentChord = chordsFound[i];
+      ++chordCount; 
+      continue; 
+    }
+    
+    if(chordsFound[i] != currentChord)
+    {
+      chordCount = 1;
+      currentChord = chordsFound[i];
+      continue;
+    }
+        
+    if(++chordCount >= 3)
+    {
+      scaleChords.push(currentChord);      
+      chordCount = 1;
+    }    
+  }
+  return scaleChords;
+}
+
+// Explore every chord pattern possibility with every note of the scale
+function getAllChordsOfScale(root, scale)
+{
+  let scaleNotes = getScaleNotes(root, scale);
+  let scaleChords = [];
+  
+  scaleNotes.forEach(note => scaleChords.push(getAllChordsOfNote(note, root, scale)));
+  
+  return scaleChords;
+}
+
+// Get all possible chords of a note in a specific scale
+function getAllChordsOfNote(note, root, scale)
+{  
+  let scaleNotes = getScaleNotes(root, scale);
+  let noteChords = [];
+  
+  ["major","minor"].forEach(voicing => 
+  {
+    let voicingIsPresent = !chordPatterns[voicing].some(edge => !scaleNotes.includes(tonnetze[note].edges[edge]))
+
+    if(voicingIsPresent) 
+    {
+      // Check for major extensions
+      let extensions = [];
+      for(let extension in chordPatterns.extensions)
+      {        
+        // Do the moves
+        let pattern = chordPatterns.extensions[extension];  
+        //let currentNote = note;
+        //pattern.forEach(move => currentNote = tonnetze[currentNote].edges[move]);
+        
+        // Check the resulting note
+        if(scaleNotes.includes(tonnetzeMove(note, pattern))) { extensions.push(extension); }          
+      }
+
+      noteChords.push({root: note, voicing: voicing, extensions: extensions});        
+    }
+  });
+
+  ["augmented", "diminished"].forEach( voicing => 
+  {      
+    let voicingIsPresent = !chordPatterns[voicing].some(pattern => 
+    {
+      // Do the moves
+      //let currentNote = note;
+      //pattern.forEach(move => currentNote = tonnetze[currentNote].edges[move]);
+
+      // Check the resulting note
+      return !scaleNotes.includes(tonnetzeMove(note, pattern));
+    });    
+
+    if(voicingIsPresent) { noteChords.push({root: note, voicing: voicing, extensions: []}); }       
+  });        
+  
+  return noteChords;
+}
+
+
+//---------------------------------------------------------------
+// Tonnetze utilities
+//---------------------------------------------------------------
+
+function getTonnetzeNodeEdges(node)
+{  
+  let edges = [];
+  for(edge in tonnetze[node].edges) { edges.push(tonnetze[node].edges[edge]); }
+  return edges;
+}
+
+
+function getEdgeNameOfNote(node, note)
+{
+  edgeName = "";
+  for(nodeEdge in tonnetze[node].edges) 
+  { 
+    if(tonnetze[node].edges[nodeEdge] === note) { edgeName = nodeEdge; } 
+  }  
+  return edgeName;  
+}
+
+function oppositeEdge(edgeName)
+{
+  switch(edgeName)
+  {
+    case "+x" : return "-x";
+    case "+y" : return "-y";
+    case "+z" : return "-z";
+    case "-x" : return "+x";
+    case "-y" : return "+y";
+    case "-z" : return "+z";
+    default : console.error("Invalid edge name");
+  }
+}
+
+function tonnetzeMove(originNode, movePattern) {
+  var currentNode = originNode;
+  movePattern.forEach(move => currentNode = tonnetze[currentNode].edges[move]);
+  return currentNode;
+}
+
+function tonnetzeRandomChord() {
+  return tonnetze[Math.floor(Math.random() * 24 + 12)];
+}
+
+function tonnetzeRandomNote() {
+  return tonnetze[Math.floor(Math.random() * 12)];
+}
