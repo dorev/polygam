0000ac0241e20d4b9ed779925305651fc060fd76
Vincent <vincent.dore.millet@protonmail.com>
2019-05-12 16:29:44 -0400

    Completed first version of polygam-chord

--- src/polygam-chord.js
+++ src/polygam-chord.js
@@ -1,4 +1,4 @@
-customElements.define("polygam-chord", class extends HTMLElement
+class PolygamChord extends HTMLElement
 {            
     constructor()
     {
@@ -8,6 +8,7 @@ customElements.define("polygam-chord", class extends HTMLElement
         // CSS style
         //--------------------------------------------------------
         let style = document.createElement('style');
+        {
         style.textContent =`
         
         .chord-container
@@ -81,16 +82,17 @@ customElements.define("polygam-chord", class extends HTMLElement
             grid-row-end      : 9;
         }
         `;  
-
+        }
         //--------------------------------------------------------
         // Custom element members
         //--------------------------------------------------------    
-        this.name       = "";
-        this.voicing    = "";
-        this.root       = 0;
-        this.octave     = 0;
-        this.inversion  = 0;
-        this.notes      = [];
+        this.name         = "";
+        this.voicing      = "";
+        this.root         = 0;
+        this.octave       = 0;
+        this.inversion    = 0;
+        this.maxInversion = 0;
+        this.notes        = [];
         
         //--------------------------------------------------------
         // Construct custom element
@@ -102,18 +104,18 @@ customElements.define("polygam-chord", class extends HTMLElement
         this.invButtonUp   = document.createElement("div");
         this.invLabel      = document.createElement("div");
         this.invButtonDown = document.createElement("div");        
-        this.octave        = document.createElement("div");
+        this.octaveName    = document.createElement("div");
         this.octButtonup   = document.createElement("div");
-        this.invLabel      = document.createElement("div");
+        this.octLabel      = document.createElement("div");
         this.octButtonDown = document.createElement("div");
 
         this.chordName     .setAttribute("class","chord-name");  
         this.invButtonUp   .setAttribute("class","inversion-button-up"); 
         this.invLabel      .setAttribute("class","inversion-label"); 
         this.invButtonDown .setAttribute("class","inversion-button-down"); 
-        this.octave        .setAttribute("class","octave");  
+        this.octaveName    .setAttribute("class","octave");  
         this.octButtonup   .setAttribute("class","octave-button-up"); 
-        this.invLabel      .setAttribute("class","octave-label"); 
+        this.octLabel      .setAttribute("class","octave-label"); 
         this.octButtonDown .setAttribute("class","octave-button-down"); 
         
         shadow.appendChild(chordName);
@@ -122,8 +124,11 @@ customElements.define("polygam-chord", class extends HTMLElement
         shadow.appendChild(invButtonDown);
         shadow.appendChild(octave);
         shadow.appendChild(octButtonup);
-        shadow.appendChild(invLabel);
+        shadow.appendChild(octLabel);
         shadow.appendChild(octButtonDown);
+
+        this.invLabel.innerHTML = "INV";
+        this.octLabel.innerHTML = "OCT";
 
         //--------------------------------------------------------
         // Setup events
@@ -141,47 +146,149 @@ customElements.define("polygam-chord", class extends HTMLElement
 
     setChord(properties)
     {
-        if(!(properties.hasOwnProperty("root") &&
-             properties.hasOwnProperty("name") &&
-             properties.hasOwnProperty("voicing") &&
-             properties.hasOwnProperty("octave")))
+        if(
+           //Check root
+           !( properties.hasOwnProperty("root")
+           && Number.isInteger(properties.root)
+           // Check voicing
+           && properties.hasOwnProperty("voicing")
+           && (properties.voicing === "major" || properties.voicing === "minor")
+           // Check octave
+           && properties.hasOwnProperty("octave") 
+           && Number.isInteger(properties.octave))
+        )
         {
-            console.error("Invalid chord set");
+            console.error("Invalid chord set, check properties");
         }
 
         this.root    = properties.root;
-        this.name    = properties.name;
         this.voicing = properties.voicing;
         this.octave  = properties.octave;
 
-        // calc notes
+        // Calculate notes
+        // Add root
+        this.notes.push(this.root + 12 * this.octave);
+
+        // Add third
+        switch(this.voicing)
+        {
+            case "major" : this.notes.push(this.notes[0] + 4); break;
+            case "minor" : this.notes.push(this.notes[0] + 3); break;
+            default : console.err("Invalid voicing"); return;
+        }        
+
+        // Add fifth
+        this.notes.push(this.notes[0] + 7);
+
+        // Iterate to reach correct inversion
+        for(let inv = 1; inv < this.inversion; ++i)
+        {
+            inversionUp();
+        }
+
+        this.updateChordName();
 
     }
 
     inversionUp()
     {     
-        // update inversion value
-        // rotate chord notes   
-        // validate bass octave
-        // update chord name
+        // Increase inversion value
+        this.inversion = this.inversion === this.maxInversion ? 1 : this.inversion + 1;
+
+        // Rotate chord notes   
+        this.notes.push(this.notes.shift());
+        
+        // Validate bass octave
+        if(this.octaveOf(this.getBass()) != this.octave)
+        {
+            // Shift chord one octave down            
+            notes = notes.map(n => n - 12);
+        }
+        
+        this.updateChordName();
     }
 
     inversionDown()
     {
+        // Decrease inversion value
+        this.inversion = this.inversion === 0 ? this.maxInversion : this.inversion - 1;
+
+        // Rotate chord notes   
+        this.notes.unshift(this.notes.pop());
         
+        // Validate bass octave
+        if(this.octaveOf(this.getBass()) != this.octave)
+        {
+            // Shift chord one octave up            
+            notes = notes.map(n => n + 12);
+        }
+
+        this.updateChordName();
+    }
+
+    updateChordName()
+    {
+        let notesName = ["C","C#","D","Eb","E","F","F#","G","Ab","A","Bb","B"];
+        this.name = `${notesName[this.notes[0] % 12]} ${this.voicing}`;
+
+        if(this.inversion != 1)
+        {
+            this.name += `/${notesName[this.notes[this.inversion-1] % 12]}`;
+        }
+
+        this.chordName.innerHTML = this.name;
     }
 
     octaveUp()
     {
-        // check no note busts octave 9
-        // update octave value
-        // increase pitch by 12
+        // Check no note busts octave 9
+        if(this.octaveOf(this.getSoprano() + 12) > 9)
+        {
+            console.err("Chords note to high to increase of an octave")
+            return;
+        }
+
+        // Update octave value
+        this.octave++;
+        this.octaveName.innerHTML = this.octave;
+
+        // Increase pitch by 12
+        notes = notes.map(n => n + 12);
     }
 
     octaveDown()
     {
-        
+        // Check no note busts octave 0
+        if(this.octaveOf(this.getBass() - 12) < 0)
+        {
+            console.err("Chords note to low to decrease of an octave")
+            return;
+        }
+
+        // Update octave value
+        this.octave--;
+        this.octaveName.innerHTML = this.octave;
+
+        // Increase pitch by 12
+        notes = notes.map(n => n - 12);
+    }
+
+    octaveOf(noteNumber)
+    {
+        return Math.floor(noteNumber / 12);
+    }
+
+    getBass()
+    {        
+        return Math.min.apply(null, this.notes);
+    }
+
+    getSoprano()
+    {
+        return Math.max.apply(null, this.notes);
     }
 
 
-});
+};
+
+customElements.define("polygam-chord", PolygamChord);
